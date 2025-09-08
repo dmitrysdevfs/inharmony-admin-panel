f059be5c1b0b6de95bad1ced14062c1453c3b6a9
Stanislav <ssovelich@gmail.com>
2025-09-08 22:07:49 +0300

    added styles CollectionList

--- src/components/ui/Table/index.js
+++ src/components/ui/Table/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cn } from '../../../lib/utils';
+import { cn } from '@/lib/utils';
 import styles from './Table.module.css';
 
 const Table = ({ columns = [], data = [], className, onRowClick }) => {
