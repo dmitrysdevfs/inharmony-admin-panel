91f4a2e7eb6ea4b98ef60e64fb885ee002486a5d
Stanislav <ssovelich@gmail.com>
2025-09-08 17:28:33 +0300

    added styles fom login form

--- src/app/auth/layout.js
+++ src/app/auth/layout.js
@@ -1,26 +1,14 @@
 export default function AuthLayout({ children }) {
   return (
-    <div
+    <main
       style={{
-        backgroundColor: '#f3f4f6',
+        minHeight: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh',
       }}
     >
-      <main
-        style={{
-          width: '100%',
-          maxWidth: '400px',
-          padding: '32px',
-          backgroundColor: 'white',
-          borderRadius: '8px',
-          boxShadow: '0 0 10px rgba(0,0,0,0.1)',
-        }}
-      >
-        {children}
-      </main>
-    </div>
+      {children}
+    </main>
   );
 }
